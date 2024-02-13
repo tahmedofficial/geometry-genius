0271a32c7b6b94fd0b49259207d2f199aa68a166
tahmedofficial <tanvirofcuo@gmail.com>
2024-02-13 19:43:57 +0600

    finish rectangle

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -2,26 +2,37 @@ function calculateTriangleArea() {
     // get triangle base
     const triangleBaseInput = document.getElementById("triangle_base");
     const triangleBaseText = triangleBaseInput.value;
-    const flotBase = parseFloat(triangleBaseText);
+    const base = parseFloat(triangleBaseText);
 
     // get triangle height
     const triangleHeightInput = document.getElementById("triangle_height");
     const triangleHeightInputValue = triangleHeightInput.value;
-    const flotHeight = parseFloat(triangleHeightInputValue);
+    const height = parseFloat(triangleHeightInputValue);
 
     // calculate triangle area
-    const area = 0.5 * flotBase * flotHeight;
-    console.log("area of the traingle is:", area);
+    const traingleArea = 0.5 * base * height;
+
     // Diaplay triangle area
-    const traingleArea = document.getElementById("triangle_area");
-    traingleArea.innerText = area;
+    const displayTriangleArea = document.getElementById("dispiay_triangle_area");
+    displayTriangleArea.innerText = traingleArea;
 
 }
 
-function calculateRectangleArea(){
+function calculateRectangleArea() {
     // get length of the rectangle
-    const rectangleLengthTag =document.getElementById("rectangle_length");
-    const rectangleLengthValue = rectangleLengthTag.valur;
-    console.log(rectangleLengthValue);
-    // const 
+    const rectangleLengthTag = document.getElementById("rectangle_length");
+    const rectangleLengthValue = rectangleLengthTag.value;
+    const length = parseFloat(rectangleLengthValue);
+
+    // get width of the rectangle
+    const rectangleWidthTag = document.getElementById("rectangle_width");
+    const rectangleWidthValue = rectangleWidthTag.value;
+    const width = parseFloat(rectangleWidthValue);
+
+    // calculate rectangle area
+    const rectangleArea = length * width;
+
+    // display rectangle area
+    const displayRectangleArea = document.getElementById("display_rectangle_area");
+    displayRectangleArea.innerText = rectangleArea;
 }
